45d773d9d887bf70c00368c89035b575c03a3be1
Yuri Primyshev <p020885@yandex.ru>
2026-04-24 11:30:56 -0400

    Add initial test infrastructure: pytest config, test cases, HTTP requests, and Docker setup

---
+++ tests/api_tests/create_user_api_test.py
@@ -0,0 +1,110 @@
+import uuid
+
+import pytest, requests
+
+
+@pytest.mark.api
+class TestApiCreateUser:
+
+    @pytest.mark.debug
+    def test_admin_can_create_user_with_valid_credentials(self):
+        response = requests.post(
+            url="http://localhost:4111/api/v1/admin/users",
+            json={
+                "username": "TestUser1",
+                "password": "TestUser1!",
+                "role": "USER"
+            },
+            headers={
+                "accept": "*/*",
+                "Authorization": "Basic YWRtaW46YWRtaW4=",
+                "Content-Type": "application/json"
+            }
+        )
+
+        assert response.status_code == 201
+        assert response.json().get("username") == "TestUser1"
+        assert response.json().get("role") == "USER"
+        password_hash = response.json().get("password")
+        assert isinstance(password_hash, str) and len(password_hash.strip()) > 0
+
+    @pytest.mark.parametrize(
+        argnames="username, password, role, error_key, error_value",
+        argvalues=[
+            # Username field validation
+            ("", "TestUser2!", "USER", "username", "Username cannot be blank"),
+            ("Te", "TestUser3!", "USER", "username", "Username must be between 3 and 15 characters"),
+            ("TestUserUserUser", "TestUser4!", "USER", "username", "Username must be between 3 and 15 characters"),
+            ("TestUser5#", "TestUser5!", "USER", "username",
+             "Username must contain only letters, digits, dashes, underscores, and dots"),
+
+            # Role field validation
+            ("TestUser6", "TestUser16", "SUPERADMIN", "role", "Role must be either 'ADMIN' or 'USER'"),
+
+            # Password field validation
+            ("TestUser7", "Seven7!", "USER", "password",
+             "Password must contain at least one digit, one lower case, one upper case, one special character, no spaces, and be at least 8 characters long"),
+            ("TestUser8", "NoSpecial1", "USER", "password",
+             "Password must contain at least one digit, one lower case, one upper case, one special character, no spaces, and be at least 8 characters long"),
+            ("TestUser9", "nouppercase1!", "USER", "password",
+             "Password must contain at least one digit, one lower case, one upper case, one special character, no spaces, and be at least 8 characters long"),
+            ("TestUser10", "NOLOWERCASE1!", "USER", "password",
+             "Password must contain at least one digit, one lower case, one upper case, one special character, no spaces, and be at least 8 characters long"),
+            ("TestUser11", "NoNumber!", "USER", "password",
+             "Password must contain at least one digit, one lower case, one upper case, one special character, no spaces, and be at least 8 characters long"),
+            ("TestUser12", "With spaces1!", "USER", "password",
+             "Password must contain at least one digit, one lower case, one upper case, one special character, no spaces, and be at least 8 characters long"),
+            ("TestUser13", "", "USER", "password", "Password cannot be blank"),
+        ]
+    )
+    def test_admin_cannot_create_user_with_invalid_credentials(self, username, password, role, error_key, error_value):
+        response = requests.request(
+            method="POST",
+            url="http://localhost:4111/api/v1/admin/users",
+            json={
+                "username": username,
+                "password": password,
+                "role": role
+            },
+            headers={
+                "accept": "*/*",
+                "Authorization": "Basic YWRtaW46YWRtaW4=",
+                "Content-Type": "application/json"
+            }
+        )
+
+        assert response.status_code == 400
+        assert error_value in response.json().get(error_key)
+
+    def test_admin_cannot_create_user_that_already_exists(self):
+        # create a user and check that the user was created
+        first_response = requests.post(
+            url="http://localhost:4111/api/v1/admin/users",
+            json={
+                "username": "TestUser111",
+                "password": "TestUser1!",
+                "role": "USER"
+            },
+            headers={
+                "accept": "*/*",
+                "Authorization": "Basic YWRtaW46YWRtaW4=",
+                "Content-Type": "application/json",
+            }
+        )
+        assert first_response.status_code == 201
+
+        second_response = requests.post(
+            url="http://localhost:4111/api/v1/admin/users",
+            json={
+                "username": "TestUser111",
+                "password": "TestUser1!",
+                "role": "USER"
+            },
+            headers={
+                "accept": "*/*",
+                "Authorization": "Basic YWRtaW46YWRtaW4=",
+                "Content-Type": "application/json",
+            }
+        )
+        assert second_response.status_code == 400
+        assert "already exists" in second_response.text
